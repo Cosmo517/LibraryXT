66e8343ef257ec0950c5e26c7d479a056a7b1dd0
Ethan Bielecki <ebielecki2003@gmail.com>
2025-01-21 01:03:34 -0500

    Better DB and pydantic models

--- backend/datatypes.py
+++ backend/datatypes.py
@@ -1,6 +1,30 @@
 from pydantic import BaseModel
+from datetime import datetime
+from typing import List, Optional
 
-class Books(BaseModel):
+# Handles dealing with Tag related data
+class TagBase(BaseModel):
+    name: str
+
+class TagCreate(TagBase):
+    pass
+
+class TagModel(TagBase):
+    id: int
+
+    class Config:
+        from_attributes = True
+
+class BookTagBase(BaseModel):
+    book_id: int
+    tag_id: int
+
+class BookTagModel(BookTagBase):
+    class Config:
+        from_attributes = True
+
+# Helps with handling Book related data
+class BookBase(BaseModel):
     id: int
     isbn: str
     title: str
@@ -8,17 +32,52 @@ class Books(BaseModel):
     publisher: str
     page_count: int
     year_published: int
-    genre: str
+    genre: int
     directory: str
+    
+class BookCreate(BookBase):
+    tags: Optional[List[TagCreate]] = []
 
-class Shelf(BaseModel):
+class BookModel(BookBase):
+    id: int
+    tags: List[TagModel] = []
+
+    class Config:
+        from_attributes = True
+
+# Deals with the Genre data
+class GenreBase(BaseModel):
+    id: int
+    name: str
+    class config:
+        from_atrributes = True
+
+# Deals with the Shelf data
+class ShelfBase(BaseModel):
     id: int
     shelf_name: str
     shelf_identifier: str
     book_id: int
     bookcase_id: int
+    class Config:
+        from_attributes = True
 
-class Bookcase(BaseModel):
+# Deals with the Bookcase data
+class BookcaseBase(BaseModel):
     id: int
     bookcase_name: str
     bookcase_identifier: str
+    
+    class Config:
+        from_attributes = True
+
+# Deals with the Book Location History data
+class BookLocationHistoryBase(BaseModel):
+    id: int
+    book_id: int
+    shelf_id: int
+    bookcase_id: int
+    timestamp: datetime
+    
+    class Config:
+        from_attributes = True
--- backend/models.py
+++ backend/models.py
@@ -1,31 +1,57 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, func
+from sqlalchemy.orm import relationship
 from database import Base
 
-# Creating books table
-class Books(Base):
-    __tablename__= 'books'
+# Creating book table
+class Book(Base):
+    __tablename__= 'book'
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    isbn = Column(String(14), nullable=False)
+    isbn = Column(String(14), nullable=False, index=True)
     title = Column(String(50), nullable=False)
     author = Column (String(50), nullable=False)
+    tags = relationship('Tag', secondary='book_tag', back_populates='books', cascade="all, delete")
     publisher = Column(String(50), nullable=False)
     page_count = Column(Integer, nullable=False)
     year_published = Column(Integer, nullable=False)
-    genre = Column(String(50), nullable=False)
+    genre_id = Column(Integer, ForeignKey('genre.id'), nullable=False)
     directory = Column(String(256), nullable=False)
+    shelf_id = Column(Integer, ForeignKey('shelf.id'), nullable=True)
 
-# Creating reading list table
+class Genre(Base):
+    __tablename__ = 'genre'
+    id = Column(Integer, primary_key=True, autoincrement=True, index=True)
+    name = Column(String(50), unique=True, nullable=False)
+
+class Tag(Base):
+    __tablename__ = 'tag'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(50), nullable=False, unique=True)
+    books = relationship('Book', secondary='book_tag', back_populates='tags')
+
+class BookTag(Base):
+    __tablename__ = 'book_tag'
+    book_id = Column(Integer, ForeignKey('book.id'), primary_key=True)
+    tag_id = Column(Integer, ForeignKey('tag.id'), primary_key=True)
+
+# Creating a shelf table to store the books on a single shelf
 class Shelf(Base):
     __tablename__= 'shelf'
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     shelf_name = Column(String(32), nullable=False)
-    shelf_identifier = Column(String(16), nullable=False)
-    book_id = Column(Integer, ForeignKey('books.id'), nullable=False)
-    bookcase_id = Column(Integer, ForeignKey('bookcase.id'), nullable=False)
+    shelf_identifier = Column(String(16), nullable=False, index=True)
+    bookcase_id = Column(Integer, ForeignKey('bookcase.id'), nullable=False, index=True)
 
 
 class Bookcase(Base):
     __tablename__ = 'bookcase'
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     bookcase_name = Column(String(64), nullable=False)
-    bookcase_identifier = Column(String(32), nullable=False)
+    bookcase_identifier = Column(String(32), nullable=False, index=True)
+
+class BookLocationHistory(Base):
+    __tablename__ = "book_location_history"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    book_id = Column(Integer, ForeignKey("book.id"), nullable=False, index=True)
+    shelf_id = Column(Integer, ForeignKey("shelf.id"), nullable=False, index=True)
+    bookcase_id = Column(Integer, ForeignKey("bookcase.id"), nullable=False, index=True)
+    timestamp = Column(DateTime, nullable=False, default=func.now())
